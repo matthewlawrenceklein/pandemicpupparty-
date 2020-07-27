f56e0ce721f3fabccc4c6ce07f62d3884c15770a
matthewlawrenceklein <mtt.l.kln@gmail.com>
2020-07-27 11:03:43 -0500

    basic views, controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :parks
   resources :dogs
   resources :users
+  root "application#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
