aa0ed3d877db8d31bbd1fb231ef7431a1c76a383
matthewlawrenceklein <mtt.l.kln@gmail.com>
2020-07-28 18:29:46 -0500

    rework auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,23 +12,23 @@ class UsersController < ApplicationController
         @user = User.new
       end 
     
-      def create 
-        user = User.create(user_params)
-    
-        if user.valid? 
-          session[:user_id] = user.id
-    
-          redirect_to user_path(user)
-        else 
-          flash[:errors] = user.errors.full_messages
-          redirect_to new_user_path
-        end 
-    
+    def create 
+      user = User.create(user_params)
+  
+      if user.valid? 
+        session[:user_id] = user.id
+  
+        redirect_to user_path(user)
+      else 
+        flash[:errors] = user.errors.full_messages
+        redirect_to new_user_path
       end 
+  
+    end 
 
     private
 
     def user_params
-        params.require(:user).permit(:name, :location, :email, :password, :password_confirmation)
+        params.require(:user).permit(:name, :location, :email, :password)
     end
 end
