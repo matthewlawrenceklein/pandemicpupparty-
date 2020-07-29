3dbc7ba6e5535696910c606a298d73f34cfb4f81
brennank14 <brennank14@gmail.com>
2020-07-29 18:43:03 -0500

    button changes, landing format

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,9 @@ Park.destroy_all
 Park.create(name: "Montrose", location: "Uptown", park_img: "https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/v1555302690/shape/mentalfloss/1dogwood.jpg?itok=cNY_3CBH")
 Park.create(name: "Oak Leaf", location: "Lincoln Park", park_img: "https://www.mesaparks.com/Home/ShowPublishedImage/23734/637044009424170000")
 Park.create(name: "Cathedral", location: "River North", park_img: "https://www.cityofcarrollton.com/Home/ShowPublishedImage/8859/635974896850130000")
-Park.create(name: "Borkly", location: "Pilsen", park_img: "https://www.cityofgreen.org/ImageRepository/Document?documentID=1341")
-Park.create(name: "Fetch Land", location: "Logan Square", park_img: "https://www.ci.staunton.va.us/Home/ShowPublishedImage/2265/636265715308100000")
+Park.create(name: "Borkly", location: "Pilsen", park_img: "https://lh3.googleusercontent.com/proxy/j2y1mNVxivrDEdhxIB-hO34uh_mHpcoofIwbWqwn6argUi8TyRk6AU-nMfBbJ4-X02Pa19igL_o5DNGNCzsnoTrtSf6pTOR7LhQ0RJh9qlQteVir4wt2T3b6wITY3A")
+Park.create(name: "Fetch ", location: "Logan Square", park_img: "https://www.nbparks.org/wp-content/uploads/2017/03/Dog_Park_10_14_028.jpg")
+Park.create(name: "Wrigley", location: "Grant Park", park_img: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQCTGeK7kvC3PCqoDyFLKPcia8_Q0G52fGKrw&usqp=CAU")
 
 # 100.times do
 #     Favorite.create(user_id: User.all.sample.id, park_id: Park.all.sample.id)
