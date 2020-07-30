76bdf3392313ee571531a9c82ebafb5c48606cf0
matthewlawrenceklein <mtt.l.kln@gmail.com>
2020-07-30 18:16:53 -0500

    changed db to postgres for heroku deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,10 +15,10 @@ Park.destroy_all
 
 Park.create(name: "Wrigley Field Dog Park", location: "Wrigleyville", park_img: "https://www.cityofsapulpa.net/ImageRepository/Document?documentID=923", has_dog_specific_area: true)
 Park.create(name: "Montrose Dog Beach", location: "Uptown", park_img: "https://i.imgur.com/5t3HJbO.jpg", has_dog_specific_area: true)
-Park.create(name: "Wicker Dog Park", location: "Wicker Park", park_img: "https://media.istockphoto.com/photos/dog-playing-at-a-dog-park-in-wicker-park-chicago-picture-id1037643198?k=6&m=1037643198&s=170667a&w=0&h=2CWMFYpBkZ33k2I3omqWE0yIkQV8749bRw4EE2wZSAE=", has_dog_specific_area: false)
+Park.create(name: "Wicker Dog Park", location: "Wicker Park", park_img: "https://media.istockphoto.com/photos/dog-playing-at-a-dog-park-in-wicker-park-chicago-picture-id1037643198?k=6&m=1037643198&s=170667a&w=0&h=2CWMFYpBkZ33k2I3omqWE0yIkQV8749bRw4EE2wZSAE=", has_dog_specific_area: true)
 Park.create(name: "Portage Dog Friendly Area", location: "Portage Park", park_img: "https://photos.bringfido.com/attractions/11359/25810_11359.jpg", has_dog_specific_area: true)
-Park.create(name: "Palmisano Park", location: "Bridgeport", park_img: "https://assets.chicagoparkdistrict.com/s3fs-public/styles/558x314/public/images/locations/Palmisano%20Park%20Natural%20Area-025-Spring071016-DSE-4177%20.jpg?itok=7hGhaBwg")
-Park.create(name: "Fred Anderson Dog Park", location: "South Loop", park_img: "https://uploads-ssl.webflow.com/577ee83897ba26ad3cc0f46d/57c621395410317a109bb5cd_03-Fred-Anderson-Park.jpg", has_dog_specific_area: false)
+Park.create(name: "Palmisano Park", location: "Bridgeport", park_img: "https://assets.chicagoparkdistrict.com/s3fs-public/styles/558x314/public/images/locations/Palmisano%20Park%20Natural%20Area-025-Spring071016-DSE-4177%20.jpg?itok=7hGhaBwg", has_dog_specific_area: false)
+Park.create(name: "Fred Anderson Dog Park", location: "South Loop", park_img: "https://uploads-ssl.webflow.com/577ee83897ba26ad3cc0f46d/57c621395410317a109bb5cd_03-Fred-Anderson-Park.jpg", has_dog_specific_area: true)
 
 # 100.times do
 #     Favorite.create(user_id: User.all.sample.id, park_id: Park.all.sample.id)
